f196c50f7468cbd7b731e005a0f222f2def73c76
Robbie Thomas <robbiekt@gmail.com>
2023-01-18 16:45:30 -0800

    refactored breedFetcher and added mocha and chai testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,19 @@
 const request = require("request");
 
-const args = process.argv.slice(2);
-
-request(
-  `https://api.thecatapi.com/v1/breeds/search/?q=${args[0]}`,
-  (error, response, body) => {
-    const data = JSON.parse(body);
-    if (error) console.log(error);
-    if (data[0] !== undefined) {
-      console.log(data[0].description);
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`,
+    (error, _, body) => {
+      const data = JSON.parse(body);
+      if (error) {
+        return callback(error);
+      }
+      if (data[0] !== undefined) {
+        return callback(null, data[0].description);
+      }
+      callback("Breed not found", null);
     }
-    console.log("Breed not found.");
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
